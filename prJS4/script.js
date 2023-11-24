4c86626416122e8f7afaa0f9d05d4928da856d8e
petrobohoslavets <126755767+petrobohoslavets@users.noreply.github.com>
2023-11-24 12:57:45 +0200

    Update script.js

--- prJS4/script.js
+++ prJS4/script.js
@@ -5,9 +5,16 @@ var ourCoords = {
     longitude: 24.733861440721554
 }
 
+let watchId = null
+
+
 function getMyLocation() {
     if(navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(displayLocation, displayError)
+        var watchButton = document.getElementById("watch");
+        watchButton.onclick = watchLocation;
+        var clearWatchButton = document.getElementById("clearWatch")
+        clearWatchButton.onclick = clearWatch;
     } else {
         alert("Oops, no geolocation support")
     }
@@ -16,6 +23,7 @@ function getMyLocation() {
 function displayLocation (position) {
     let latitude = position.coords.latitude;
     let longitude = position.coords.longitude;
+    console.log(latitude, longitude)
     let div = document.getElementById('location')
     div.innerHTML = `You are at Lalitude: ${latitude}, Longitude: ${longitude}`
     div.innerHTML += ` (with ${position.coords.accuracy} meters accuracy)`
@@ -58,4 +66,15 @@ function computeDistance(startCoords, destCoords) {
 function degreesToRadians(degrees) {
     let radians = (degrees * Math.PI)/100;
     return radians;
+}
+
+function watchLocation() {
+    watchId = navigator.geolocation.watchPosition(displayLocation, displayError)
+}
+
+function clearWatch() {
+    if(watchId) {
+        navigator.geolocation.clearWatch(watchId)
+        watchId = null;
+    }
 }
